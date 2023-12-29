27541281fa68b3943158e0672a3c8b8d52dd33e5
shiyasmohd <shiyasjaseena124@gmail.com>
2023-12-29 14:57:08 +0530

    build err corrected

--- src/app/address/[address]/page.tsx
+++ src/app/address/[address]/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 import { useState } from "react"
-//@ts-ignore
-import { CircleLoader } from "react-awesome-loaders";
 export default function FetchDetails() {
 
     const [data, setData] = useState(1)
@@ -11,13 +9,7 @@ export default function FetchDetails() {
             {
                 data == null ?
                     <div className="flex justify-center items-center flex-col h-screen">
-                        <CircleLoader
-                            meshColor={"#6366F1"}
-                            lightColor={"#E0E7FF"}
-                            duration={1.5}
-                            desktopSize={"90px"}
-                            mobileSize={"64px"}
-                        />
+                        Loading
                     </div>
                     :
                     <div className="flex justify-center items-center h-screen">
