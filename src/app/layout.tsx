beae3da3d09617e92140d89a94fc1d42bd09923f
shiyasmohd <shiyasjaseena124@gmail.com>
2023-12-30 01:48:46 +0530

    meta data added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <script
-        type="module"
-        defer
-        src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/spiral.js"
-      ></script>
+      <head>
+
+        <title>ETH Wrapped 2023</title>
+        <meta name="description" content="ETH Wrapped 2023 by Shiyas & Hashir" />
+
+        <meta property="og:url" content="https://ethwrapped.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="ETH Wrapped 2023" />
+        <meta property="og:description" content="ETH Wrapped 2023 by Shiyas & Hashir" />
+        <meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/35f9c73e-6a2a-45d6-a9d2-2a74c0b2bd35.png?token=jKEP5Ffr9V0W-QvgINBsGndLs7UtS3HzRImhNdeDbX8&height=574&width=1200&expires=33239881002" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ethwrapped.vercel.app" />
+        <meta property="twitter:url" content="https://ethwrapped.vercel.app/" />
+        <meta name="twitter:title" content="ETH Wrapped 2023" />
+        <meta name="twitter:description" content="ETH Wrapped 2023 by Shiyas & Hashir" />
+        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/35f9c73e-6a2a-45d6-a9d2-2a74c0b2bd35.png?token=jKEP5Ffr9V0W-QvgINBsGndLs7UtS3HzRImhNdeDbX8&height=574&width=1200&expires=33239881002" />
+      </head>
       <body className={inter.className}>
         {children}
         <Footer />
