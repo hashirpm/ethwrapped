7a39a70d366e2b73ea62a7d124607664ee9ab8cd
shiyasmohd <shiyasjaseena124@gmail.com>
2023-12-30 20:50:47 +0530

    name change to xdev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,19 +21,19 @@ export default function RootLayout({
 
 
         <title>ETH Wrapped 2023</title>
-        <meta name="description" content="ETH Wrapped 2023 by Shiyas & Hashir" />
+        <meta name="description" content="ETH Wrapped 2023 | Built by XDev" />
 
         <meta property="og:url" content="https://www.ethwrapped.xyz/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="ETH Wrapped 2023" />
-        <meta property="og:description" content="ETH Wrapped 2023 by Shiyas & Hashir" />
+        <meta property="og:description" content="ETH Wrapped 2023 | Built by XDev" />
         <meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/c44451d3-7012-4c9e-88ea-813329be06e1.png?token=M0YaKkhxfy58Vn2x_EWDueSZcK5Rj8kbC0p7WzoW0GQ&height=770&width=1200&expires=33239920044" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="ethwrapped.xyz" />
         <meta property="twitter:url" content="https://www.ethwrapped.xyz/" />
         <meta name="twitter:title" content="ETH Wrapped 2023" />
-        <meta name="twitter:description" content="ETH Wrapped 2023 by Shiyas & Hashir" />
+        <meta name="twitter:description" content="ETH Wrapped 2023 | Built by XDev" />
         <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/c44451d3-7012-4c9e-88ea-813329be06e1.png?token=M0YaKkhxfy58Vn2x_EWDueSZcK5Rj8kbC0p7WzoW0GQ&height=770&width=1200&expires=33239920044" />
 
       </head>
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className=" h-[100px] mt-20 relative flex flex-col items-center max-w-6xl px-4 pt-6 mx-auto before:absolute before:h-px before:inset-x-0 before:top-0 before:bg-gradient-to-r before:from-transparent before:via-gray-50/20">
             <p className={`text-sm text-gray-400`}>
-                Made by <Link href="https://twitter.com/0xShiyasmohd" className="text-gray-300">@shiyasmohd</Link> & <Link href="https://twitter.com/0xhashir" className="text-gray-300">@hashirpm</Link>
+                Built by <Link href="https://twitter.com/xdevlabs" className="text-gray-300">@XDev</Link>
             </p>
         </div>
     )
