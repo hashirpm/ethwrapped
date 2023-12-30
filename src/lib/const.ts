16554ae458b86aa79ca64b7283f7beec03c8e17b
shiyasmohd <shiyasjaseena124@gmail.com>
2023-12-30 13:53:26 +0530

    .

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -20,5 +20,7 @@ export const aviano = localFont({
 
 
 export const shortWalletAddress = (address?: string) => {
+  if (address == "N/A") return "N/A"
   if (address) return address.slice(0, 5) + "..." + address.slice(-4,)
+  return "N/A"
 }
