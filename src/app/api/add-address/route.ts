b188523825eaafb72778384eafeb78cfb1dd3301
shiyasmohd <shiyasjaseena124@gmail.com>
2023-12-31 10:17:12 +0530

    .

--- src/app/api/add-address/route.ts
+++ src/app/api/add-address/route.ts
@@ -2,21 +2,28 @@ import { createClient } from "@supabase/supabase-js";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const { searchParams } = new URL(req.url as string);
-    const walletAddress = searchParams.get("address") as string;
-    const supabase = createClient(process.env.SUPABASE_PROJECT_URL as string, process.env.SUPABASE_API_KEY as string)
-    console.log("inside server");
-    const data = await supabase
-        .from('wallets')
-        .insert([
-            { address: walletAddress },
-        ])
-        .select("*")
 
-    if (data.error == null) {
-        return NextResponse.json({ status: true, data })
-    } else {
-        return NextResponse.json({ status: false })
+    try {
+
+        const { searchParams } = new URL(req.url as string);
+        const walletAddress = searchParams.get("address") as string;
+        const supabase = createClient(process.env.SUPABASE_PROJECT_URL as string, process.env.SUPABASE_API_KEY as string)
+        console.log("inside server");
+        console.log(walletAddress);
+        const data = await supabase
+            .from('wallets')
+            .insert([
+                { address: walletAddress },
+            ])
+            .select("*")
+        console.log(data)
+        if (data.error == null) {
+            return NextResponse.json({ status: true, data })
+        } else {
+            return NextResponse.json({ status: false })
+        }
+    } catch (err) {
+        console.log(err)
     }
 
 
